2907dfce0e062617103277882e7bc21dba69c779
Rachel Evans <revans@zendesk.com>
2023-11-17 09:03:31 +0100

    Use graphql to read the manifest tree

--- run.rb
+++ run.rb
@@ -19,11 +19,20 @@ writer = CommentWriter.new(event, github_client, magic_text)
 # HEAD is the PR's merge commit, not the PR's branch's head
 # Assuming we're merging into main, then
 # HEAD^1 is `main`, and HEAD^2 is the branch we're merging.
-# HEAD^1..HEAD is therefore the change introduced by this PR.
-added_or_removed_files = `git diff --stat --name-only -z --diff-filter=AD --no-rename HEAD^1..HEAD`
-$CHILD_STATUS.success? or raise 'git diff failed'
 
-active = added_or_removed_files.split("\0").any? { |path| path.start_with?('kubernetes/manifests/') }
+left, right = ['base', 'head'].map do |key|
+  branch = event.fetch('pull_request').fetch(key)
+  owner = branch.fetch('repo').fetch('owner').fetch('login')
+  name = branch.fetch('repo').fetch('name')
+  commit = branch.fetch('sha')
+
+  p [key, owner, name, commit]
+
+  yaml_files = `gh api graphql -F owner=#{owner} -F name=#{name} -F manifestsTree=#{commit}:kubernetes/manifests -f query=$( cat k8s-manifests-tree.gql ) | jq -r .. | grep yml$`
+  yaml_files.sort.tap { |x| p x }
+end
+
+active = (left != right)
 
 if active
   puts 'Added / removed manifests detected'
